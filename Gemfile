89097ecfd0f4c55dad03336657de5078c6f91c93
Alex Jarvis <alxjrvs@gmail.com>
2016-06-28 14:17:24 -0400

    Initial

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.2.0'
+gem 'jquery-rails'
+
+group :test do 
+  gem 'rspec-rails'
+  gem 'capybara'
+end
---
+++ spec/features/welcome_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+feature "Building a Feature test for my Cool new app" do 
+  scenario "has the text I expect it to have" do
+    visit '/'
+
+    expect(page).to have_content "Hello, World!"
+  end
+end
